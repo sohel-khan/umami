f517f7ac836e803d0d073afd80070d8e2f70108b
Sohel Khan <khan.sohel005@gmail.com>
2025-07-16 17:30:31 +0530

    auto

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -21,7 +21,7 @@ export default async function ({ children }) {
 
 export const metadata: Metadata = {
   title: {
-    template: '%s | Umami',
-    default: 'Umami',
+    template: '%s | Recaho',
+    default: 'Recaho',
   },
 };
